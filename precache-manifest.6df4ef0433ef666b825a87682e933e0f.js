52fbd43c92a07e60c828abe22effb499db3f111d
willisntannpc <wn.mugoto@gmail.com>
2024-11-06 12:58:54 +0300

    Updates

---
+++ precache-manifest.6df4ef0433ef666b825a87682e933e0f.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a04bad9eda3aee035f180346009de533",
+    "url": "/rct-hks-hogwild/index.html"
+  },
+  {
+    "revision": "f8bf680b721360bdf4c2",
+    "url": "/rct-hks-hogwild/static/css/2.3c4bf932.chunk.css"
+  },
+  {
+    "revision": "be61edcd5761fc25d0af",
+    "url": "/rct-hks-hogwild/static/css/main.f2d717e1.chunk.css"
+  },
+  {
+    "revision": "f8bf680b721360bdf4c2",
+    "url": "/rct-hks-hogwild/static/js/2.cc8510da.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/rct-hks-hogwild/static/js/2.cc8510da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be61edcd5761fc25d0af",
+    "url": "/rct-hks-hogwild/static/js/main.81845f62.chunk.js"
+  },
+  {
+    "revision": "c34e1d725c3a808e8d96",
+    "url": "/rct-hks-hogwild/static/js/runtime-main.649a87e3.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/rct-hks-hogwild/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/rct-hks-hogwild/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/rct-hks-hogwild/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/rct-hks-hogwild/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/rct-hks-hogwild/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/rct-hks-hogwild/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/rct-hks-hogwild/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/rct-hks-hogwild/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/rct-hks-hogwild/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/rct-hks-hogwild/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/rct-hks-hogwild/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/rct-hks-hogwild/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/rct-hks-hogwild/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/rct-hks-hogwild/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/rct-hks-hogwild/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/rct-hks-hogwild/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "00a18b450a374658e2e85867e5d012fa",
+    "url": "/rct-hks-hogwild/static/media/porco.00a18b45.png"
+  }
+]);
